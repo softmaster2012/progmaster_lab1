58a866602d93265536b13ecb511f7f3fac1e40ac
Nerguth <Nerguth@192.168.2.100>
2014-12-31 14:28:00 +0200

    Оперативные изменения - v.1.02 - 31.12.2014 14:30

--- tpl/panels/header.php
+++ tpl/panels/header.php
@@ -1,1 +1,10 @@
-<h4>Header</h4>
+<table border="0">
+    <tr>
+        <td class="side">Logo</td>
+        <td>
+            <h1>Лаборатория программирования</h1>
+            <h2>Киевский филиал Компьютерной Академии "Шаг"</h2>
+        </td>
+        <td class="side">Info</td>
+    </tr>
+</table>
